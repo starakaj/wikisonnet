e325934228fab44eb59c3dae6f4f7bf93e97d502
Sam Tarakajian <starakajian@gmail.com>
2015-12-09 15:08:19 -0500

    Adding changes to the dotmatrix file and the pyfiglet requirement

--- server/dotmatrix.py
+++ server/dotmatrix.py
@@ -1,17 +1,11 @@
 from __future__ import print_function
 import os
-import ascii
 import urllib
 from pyfiglet import figlet_format
 
 tc = "  "
 
 def printPoem(title, poem_lines, imageURL=None):
-    if imageURL is not None:
-        ext = imageURL.split('.')[-1]
-        urllib.urlretrieve(imageURL, '/tmp/img.' + ext)
-        img_as_text = ascii.covertImageToAscii('/tmp/img.'+ext, 80, 0.43, False)
-
     with open('/tmp/poem.txt', 'w') as f:
         if imageURL is not None:
             for row in img_as_text:
