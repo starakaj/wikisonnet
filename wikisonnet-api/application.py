557c4ad3eb3f5816754f5bd1806b15dd351bf7d1
Cassie Tarakajian <ctarakajian@gmail.com>
2015-12-30 16:59:48 -0500

    remove debugger statement, update requirements.txt

--- wikisonnet-api/application.py
+++ wikisonnet-api/application.py
@@ -21,7 +21,7 @@ from flask import request, Response, jsonify, session
 import wikiconnector
 from multiprocessing import Manager, Queue, cpu_count, Process
 from flask.ext.cors import CORS
-from IPython import embed
+# from IPython import embed
 
 # Default config vals
 THEME = 'default' if os.environ.get('THEME') is None else os.environ.get('THEME')
