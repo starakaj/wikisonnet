e9e17e1bb0711c442b3accae9f776282371271bd
Sam Tarakajian <starakajian@gmail.com>
2016-01-14 19:19:39 -0500

    removing the node-client module, which wow we sure as fuck weren't using anymore

--- node-client/app.js
+++
@@ -1,6 +0,0 @@
-var React = require('react');
-var Poem = require('./views/index.jsx');
-
-var books = JSON.parse(document.getElementById('initial-data').getAttribute('data-json'));
-var fucks = function() {console.log("Fuck")}
-React.render(<Poem/>, document.getElementById('container'));
